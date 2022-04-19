ba935c3e34de13fd83b714913862bd4b4fc2d2b7
Konstantinos Kraniotis <konstantinos.kraniotis@eurodyn.com>
2022-04-19 12:53:23 +0300

    changes to repositories

--- src/main/java/com/example/exams/repository/FilmRepository.java
+++ src/main/java/com/example/exams/repository/FilmRepository.java
@@ -1,9 +1,9 @@
 package com.example.exams.repository;
 
-import com.example.exams.domain.Program;
+import com.example.exams.domain.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProgramRepository extends JpaRepository<Program, Long> {
+public interface FilmRepository extends JpaRepository<Film, Long> {
 }
---
+++ src/main/java/com/example/exams/repository/ShowRepository.java
@@ -0,0 +1,9 @@
+package com.example.exams.repository;
+
+import com.example.exams.domain.Show;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShowRepository extends JpaRepository<Show, Long> {
+}
